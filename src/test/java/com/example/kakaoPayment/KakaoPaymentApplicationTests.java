a05d0ea1d9e74c75ded6a0cecc480fa84a0918e7
신동현 <donghyeun02@gmail.com>
2023-11-20 18:10:42 +0900

    feat : Initial Setting

---
+++ src/test/java/com/example/kakaoPayment/KakaoPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaoPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
